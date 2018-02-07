8374a4bd94c7f814ff334b98ed9799d9b00bda5a
shawn <axe2781@gmail.com>
2018-02-07 15:11:54 -0500

    fb log

--- ResortProperties/ResortProperties/Startup.cs
+++ ResortProperties/ResortProperties/Startup.cs
@@ -41,39 +41,43 @@ namespace ResortProperties
             {
                 googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
                 googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
-            }
-            );
+            })
+            .AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            });
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
-            services.AddScoped<IResortPostsRepo,ResortRepoFs>();
+            services.AddScoped<IResortPostsRepo, ResortRepoFs>();
             services.AddMvc();
         }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
             {
-                if (env.IsDevelopment())
-                {
-                    app.UseBrowserLink();
-                    app.UseDeveloperExceptionPage();
-                    app.UseDatabaseErrorPage();
-                }
-                else
-                {
-                    app.UseExceptionHandler("/Home/Error");
-                }
-
-                app.UseStaticFiles();
-
-                app.UseAuthentication();
-
-                app.UseMvc(routes =>
-                {
-                    routes.MapRoute(
-                        name: "default",
-                        template: "{controller=Home}/{action=Index}/{id?}");
-                });
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
